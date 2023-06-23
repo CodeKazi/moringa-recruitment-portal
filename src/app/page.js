d5efea12d657e46e66f39ce9b50643436e892e0a
brk-a <fnjakai@outlook.com>
2023-06-23 10:01:23 +0000

    dashboard: social proof - done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import ResetPassword from "./resetPassword2"
+// import ResetPassword from "./resetPassword2"
 // import ResetPassword from "./resetPassword1"
-// import Dashboard from "./dashboard"
+import Dashboard from "./dashboard"
 // import SignIn from "./signin"
 
 export default function Home() {
@@ -8,8 +8,8 @@ export default function Home() {
     <>
     <div className="container mx-auto h-full">
       {/* <SignIn/> */}
-      <ResetPassword/> {/* default export in resetPassword1 and resetPassword2 is named ResetPassword */}
-      {/* <Dashboard/> */}
+      {/*<ResetPassword/>*/} {/* default export in resetPassword1 and resetPassword2 is named ResetPassword */}
+      <Dashboard/>
     </div>
     </>)
 
