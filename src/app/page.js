225b1ec1d98c3a5a162c4af566c8719035f06627
Timoh97 <timohmugendi@gmail.com>
2023-06-26 20:32:51 +0300

    create grid items

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 // import ResetPassword from "./resetPassword2"
 // import ResetPassword from "./resetPassword1"
-// import Dashboard from "./dashboard"
+import Dashboard from "./dashboard"
 // import SignIn from "./signin"
 import Register from "./register"
+import Draftjobs from './draftjobs'
+
 
 export default function Home() {
     return (
     <>
-      <Register/>
-//     <div className="container mx-auto h-full">
-//        <ResetPassword/>
-//     </div>
+   
+      <Draftjobs/>
+
     </>)
 }
