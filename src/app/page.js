046203ffc7ce4e636a31bca6f7dec04e40882fa1
MaureenWanjiku <nyingimaureen19@gmail.com>
2023-06-27 12:53:52 +0300

    added apply modal page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 //import Register from "./register"
-import JobListing from "./job_listing"
+// import JobListing from "./job_listing"
 // import JobInfo from "./job_listing2"
+ import ApplyModal from "./applymodal"
 
 export default function Home() {
   return (
     <main >
-      {/* <Register /> */}
+      <ApplyModal /> 
+       {/* <Register /> */}
       {/* <JobInfo /> */}
-      <JobListing />
+      {/* <JobListing /> */}
     </main>
   )
 }
