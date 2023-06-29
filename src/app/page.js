efb8f49c33e61033d61dd586ab3751ae2cbefb7d
Chalie Wizz <60155767+Chal13W1zz@users.noreply.github.com>
2023-06-29 19:49:35 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,23 @@
-//import Register from "./register"
-//  import JobListing from "./job_listing"
- import JobInfo from "./job_listing2"
-//  import ApplyModal from "./applymodal"
+// import Register from "./register"
+// import JobListing from "./job_listing"
+// import JobInfo from "./job_listing2"
+// import ApplyModal from "./applymodal"
+// import ResetPassword from "./resetPassword2"
+// import ResetPassword from "./resetPassword1"
+// import Dashboard from "./dashboard"
+// import AlumniProfileMain from "./alumniProfileMain"
+// import JobInfo from "./job_listing2"
 // import AlumniNavbar from "../../components/AlumniNavbar"
 // import AlumniFooter from "../../components/AlumniFooter"
+import SignIn from "./signin"
 
 export default function Home() {
   return (
-    <main >
-      {/* <AlumniNavbar /> */}
-      {/* <ApplyModal />  */}
-       {/* <Register /> */}
-      <JobInfo />
-      {/* <AlumniFooter /> */}
-      {/* <JobListing /> */}
+    <main>
+    <div className="container mx-auto h-full">
+       <SignIn/> 
+      <Dashboard/>
+    </div>
     </main>
   )
 }
