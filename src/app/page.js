b6c6c414aa4a3b256ffa8aa8f1ce665e2c79356c
MaureenWanjiku <nyingimaureen19@gmail.com>
2023-07-03 14:21:27 +0300

    fixed page.js conflict

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@
 // import AlumniNavbar from "../../components/AlumniNavbar"
 // import AlumniFooter from "../../components/AlumniFooter"
 // import AppliedJobs from "./appliedjobs"
-import SignIn from "./signin"
+ import SignIn from "./signin"
 
 export default function Home() {
   return (
