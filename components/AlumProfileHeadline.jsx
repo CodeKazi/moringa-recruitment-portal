0852162878f9ae73b07d3f8964b89e3e9e10bbaa
brk-a <fnjakai@outlook.com>
2023-07-05 10:11:10 +0000

    resolve `image` issue

--- components/AlumProfileHeadline.jsx
+++ components/AlumProfileHeadline.jsx
@@ -11,7 +11,7 @@ const AlumProfileHeadline = ({
   return (
         <div className='flex flex-row items-center ml-[165px] gap-10'>
           <div className='flex basis-2/10 justify-center items-center w-3/12'>
-            <img src={image} className='flex-shrink-0 rounded-full'/>
+            <Image height={150} width={150} alt='logo' src={image} className='flex-shrink-0 rounded-full'/>
           </div>
           <div className='flex flex-col basis-7/10 w-8/12'>
             <h1 className='text-3xl font-semibold text-white'>{name}</h1>
