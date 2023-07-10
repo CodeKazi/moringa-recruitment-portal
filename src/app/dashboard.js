567ab6281c1d91b1d44997102d00e4359d09991f
brk-a <fnjakai@outlook.com>
2023-07-10 12:36:43 +0000

    fills page

--- src/app/dashboard.js
+++ src/app/dashboard.js
@@ -142,7 +142,7 @@ const Dashboard = () => {
 
                 {/* Footer */}
                 <div className='bg-[#484848] w-full h-[498px] flex flex-col flex-shrink-0'>
-                    <div className='bg-moringa_blue opacity-50 w-[1440px] h-[497.712px] inline-flex flex-row flex-shrink-0'>
+                    <div className='bg-moringa_blue opacity-50 w-full h-[497.712px] inline-flex flex-row flex-shrink-0'>
                         <div className='basis-2/5 w-full'>
                             <div className='mt-[69px] ml-[66px]'>
                                 <ul className='flex flex-col items-center'>
