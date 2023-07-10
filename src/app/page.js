d6b0980a0a1e3edb4f321bd4f026a700a23e9e41
MaureenWanjiku <nyingimaureen19@gmail.com>
2023-07-10 16:10:40 +0300

    exposed jobinfo in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,16 @@
 // import ResetPassword from "./resetPassword1"
 // import Dashboard from "./dashboard"
 // import AlumniProfileMain from "./alumniProfileMain"
-//  import JobInfo from "./job_listing2"
+ import JobInfo from "./job_listing2"
 // import AlumniNavbar from "../../components/AlumniNavbar"
 // import AlumniFooter from "../../components/AlumniFooter"
-import SignIn from "./signin"
+// import SignIn from "./signin"
 
 export default function Home() {
   return (
     <main>
     <div className="container mx-auto h-full">
-       <SignIn/> 
+    <JobInfo />
     </div>
     </main>
   )
