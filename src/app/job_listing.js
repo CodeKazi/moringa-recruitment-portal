7702d99663cfec5ca7fe1fc6d1368638b29016ca
MaureenWanjiku <nyingimaureen19@gmail.com>
2023-07-11 09:21:49 +0300

    removed white spaces in the main job listing page

--- src/app/job_listing.js
+++ src/app/job_listing.js
@@ -1,164 +1,178 @@
 import React from 'react'
 import "./job_listing.css"
+import Image from 'next/image'
 import { recentJobs } from '../../cardData/recentjobs'
 
 const JobListing = () => {
   return (
     <div>
-
+      
       <nav>
         <div className='logo mt-1'>
-          <img src='/assets/moringa0.png' alt='Moringa School' />
+          <Image src='/assets/moringa0.png' alt='Moringa School' width='100' height='100' />
         </div>
 
-        <div className='home'>
+        <div className='home text-2xl font-normal'>
           <span className='mt-4'>Home</span>
-          <span className='ml-4 mt-4'>Jobs</span>
+          <span className='ml-10 mt-4'>Jobs</span>
 
         </div>
 
         <div className='profile-btn'>
-          <button>Profile</button>
+          <button className='bg-moringa_blue text-xl font-bold py-3.5 px-7 mt-4 rounded-md text-white cursor-pointer'>Profile</button>
         </div>
       </nav>
 
-      <section className='top-section'>
+      <section className='top-section p-16 bg-moringa_blue text-white'>
         <div className='find'>
-          <p className='text-white mt-2'>Find your job</p>
+          <p className='text-white text-4xl font-medium -mt-12 mb-4'>Find your job</p>
         </div>
 
-        <div className='controls mt-8'>
-          <p className='job'>Job title or keyword <hr /></p>
-          <p className='location'>Location <hr /></p>
-          <button className='search mt-0'>Search</button>
+        <div className='controls mt-8 text-xl font-medium'>
+          <p className='job '>Job title or keyword <hr className='mt-2' /></p>
+          <p className='location'>Location <hr className='mt-2' /></p>
+          <button className='bg-moringa_orange mt-0 font-semibold text-lg py-2.5 px-10 rounded-md cursor-pointer'>Search</button>
         </div>
 
       </section>
 
-      <section className='sidebar cursor-pointer'>
-        <p className='categories'>Categories</p>
-        <div className='frontend'>
+      <section className='flex'>
+
+      <section className='sidebar cursor-pointer flex flex-col ml-20'>
+        <p className='categories text-xl font-medium mt-4'>Categories</p>
+
+        <div className='text-lg font-medium mb-4 ml-4 mt-2'>
 
           <input type='radio'></input>
           <label className='ml-2 '>Frontend (150)</label>
 
         </div>
 
-        <div className='specific'>
+        <div className='specific text-lg font-normal ml-8'>
+
           <ul className='first'>
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>React Js (39)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>React Js (39)</label></li>
 
-            <li> <input type='checkbox'></input>
-              <label className='ml-2'>Next Js (40)</label></li>
+            <li className='mb-2'> <input type='checkbox'></input>
+              <label className='ml-3 '>Next Js (40)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>Svelte (20)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>Svelte (20)</label></li>
 
-            <li> <input type='checkbox'></input>
-              <label className='ml-2'>Angular (54)</label></li>
+            <li className='mb-2'> <input type='checkbox'></input>
+              <label className='ml-3'>Angular (54)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>Swift (54)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>Swift (54)</label></li>
 
-            <li> <input type='checkbox'></input>
-              <label className='ml-2'>JavaScript (105)</label></li>
+            <li className='mb-2'> <input type='checkbox'></input>
+              <label className='ml-3'>JavaScript (105)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>TypeScript (5)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>TypeScript (5)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>Kotlin (3)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>Kotlin (3)</label></li>
 
-            <li> <input type='checkbox'></input>
-              <label className='ml-2'>Java (8)</label></li>
+            <li className='mb-2'> <input type='checkbox'></input>
+              <label className='ml-3'>Java (8)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>Dart (3)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>Dart (3)</label></li>
 
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>React Native (19)</label></li>
+            <li className='mb-2'><input type='checkbox'></input>
+              <label className='ml-3'>React Native (19)</label></li>
 
-            <li> <input type='checkbox'></input>
-              <label className='ml-2'>Flutter (7)</label></li>
+            <li className='mb-2'> <input type='checkbox'></input>
+              <label className='ml-3'>Flutter (7)</label></li>
           </ul>
         </div>
-        <div className='types'>
-          <ul>
-            <li><input type='radio'></input>
-              <label className='ml-2'>Backend (200)</label></li>
-            <li><input type='radio'></input>
-              <label className='ml-2'>Fullstack (80)</label></li>
-            <li><input type='radio'></input>
+        <div className='types mt-4 text-lg font-medium'>
+          <ul className='mb-3'>
+            <li className='mb-2'><input type='radio'></input>
+              <label className='ml-2 '>Backend (200)</label></li>
+            <li className='mb-2'><input type='radio'></input>
+              <label className='ml-2 '>Fullstack (80)</label></li>
+            <li className='mb-2'><input type='radio'></input>
               <label className='ml-2'>UI/UX (33)</label></li>
-            <li><input type='radio'></input>
+            <li className='mb-2'><input type='radio'></input>
               <label className='ml-2'>Cloud (15)</label></li>
-            <li> <input type='radio'></input>
+            <li className='mb-2'> <input type='radio'></input>
               <label className='ml-2'>Blockchain(17)</label></li>
-            <li><input type='radio'></input>
+            <li className='mb-2'><input type='radio'></input>
               <label className='ml-2'>Mobile (41)</label></li>
           </ul>
 
         </div>
 
         <div>
-          <p className='level'>Job Level</p>
+          <p className='text-xl font-medium mt-4'>Job Level</p>
           <ul>
-            <li><input type='checkbox'></input>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
               <label className='ml-2'>Entry Level</label></li>
-            <li> <input type='checkbox'></input>
+            <li className='text-lg font-normal mb-2'> <input type='checkbox'></input>
               <label className='ml-2'>Mid Level</label></li>
-            <li><input type='checkbox'></input>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
               <label className='ml-2'>Senior Level</label></li>
           </ul>
 
         </div>
 
         <div>
-          <p className='jobtype'>On-Site/Remote</p>
+          <p className='text-xl font-medium mt-3'>Job Type</p>
           <ul>
-            <li><input type='checkbox'></input>
-              <label className='ml-2'>Remote</label></li>
-            <li><input type='checkbox'></input>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
+              <label className='ml-2'>Full-time</label></li>
+            <li className='text-lg font-normal mb-2'> <input type='checkbox'></input>
+              <label className='ml-2'>Contract</label></li>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
+              <label className='ml-2'>Internship</label></li>
+          </ul>
+
+        </div>
+
+        <div>
+          <p className=' text-xl font-medium mt-4'>On-Site/Remote</p>
+          <ul>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
+              <label className='ml-2 '>Remote</label></li>
+            <li className='text-lg font-normal mb-2'><input type='checkbox'></input>
               <label className='ml-2'>Hybrid</label></li>
-            <li> <input type='checkbox'></input>
+            <li className='text-lg font-normal mb-2'> <input type='checkbox'></input>
               <label className='ml-2'>On-Site</label></li>
           </ul>
 
         </div>
       </section>
 
-      <section className='center'>
+      <section className='center ml-8'>
         <div>
-          <div className='center-titles'>
-            <h5 className='text-xl font-medium'>All Jobs</h5>
-            <p className='flex text-lg font-medium'>Sort By  <span className='ml-1 mt-0.5'><img src='./icons/majesticons_chevron-down-line.png' /></span></p>
+          <div className='center-titles flex justify-around mt-4'>
+            <h5 className='text-2xl font-medium mr-20'>All Jobs</h5>
+            <p className='flex text-lg font-medium ml-40'>Sort By  <span className='ml-1 mt-0.5'><img src='./icons/majesticons_chevron-down-line.png' /></span></p>
           </div>
 
           <div className='grid-container p-3'>
 
             {recentJobs.map((card, i) => (
-              <div key={i} className='grids flex' >
+              <div key={i} className='grids flex border border-gray-400' >
 
                 <div className='flex items-center'>
-                  <img src={card.image} alt='Logo' />
+                  <Image src={card.image} alt='Logo' width='81' height='61'  className='ml-5 cursor-pointer'/>
                  
                 </div>
-                <div className='ml-7 mt-3'>
-                  <p className='text-lg font-medium'>{card.title}</p>
-                  <p className='text-base mb-2'>{card.location}</p>
+                <div className='ml-10 mt-3'>
+                  <p className='text-xl font-semibold mb-2'>{card.title}</p>
+                  <p className='text-lg font-medium mb-3'>{card.location}</p>
                   
-                   <div>
+                   <div className='mb-3'>
                     {card.tags.map((tag, index) =>(
-                      <span key={index} className={`mr-3 mb-2 p-px rounded-3xl ${index % 2 === 0 ?  'bg-gray-500' : 'bg-orange-500' }`}>{tag}</span>
-
-              
+                      <span key={index} className={`text-lg font-normal cursor-pointer mr-3  py-1 px-2.5 rounded-3xl ${index % 2 === 0 ?  'bg-gray-500' : 'bg-orange-500' }`}>{tag}</span>
 
                     ))}
                    </div>
                     
-                  
-
                 </div>
               </div>
 
@@ -168,27 +182,29 @@ const JobListing = () => {
         </div>
       </section>
 
-      <div className='pagination my-1'>
-        <a href='#'><img src='./icons/gg_push-chevron-left.png' /></a>
-        <a href='#'><img src='./icons/gg_chevron-left.png' /></a>
-        <a href='#' className='pl-2 font-medium'>1</a>
-        <a href='#' className='pl-2 font-medium'>2</a>
-        <a href='#' className='pl-2 font-medium'>3</a>
-        <a href='#'><img src='./icons/gg_chevron-right.png' /></a>
-        <a href='#'><img src='./icons/gg_push-chevron-right.png' /></a>
+      </section>
+
+      <div className='pagination my-3'>
+        <a href='#' className='py-1.5 px-3'><Image src='./icons/gg_push-chevron-left.png ' width='24' height='24' /></a>
+        <a href='#' className='py-1.5 px-3 ml-4'><Image src='./icons/gg_chevron-left.png' width='25' height='25' /></a>
+        <a href='#' className='py-1 px-5 ml-4 text-xl font-medium'>1</a>
+        <a href='#' className='py-1 px-5 ml-4 text-xl font-medium'>2</a>
+        <a href='#' className='py-1 px-5 ml-4 text-xl font-medium'>3</a>
+        <a href='#' className='py-1.5 px-3 ml-4'><Image src='./icons/gg_chevron-right.png' width='25' height='25'/></a>
+        <a href='#' className='py-1.5 px-3 ml-4'><Image src='./icons/gg_push-chevron-right.png' width='24' height='24'/></a>
       </div>
 
-      <section className='footer'>
-        <div className='footer-content'>
-          <p className='footer-title'>Moringa School</p>
-          <p className='footer-location'>Ngong Lane, Ngong Lane Plaza, 1st Floor, Nairobi Kenya</p>
+      <section className='footer bg-moringa_blue text-white p-14'>
+        <div className='footer-content -mt-12'>
+          <p className='footer-title mb-1'>Moringa School</p>
+          <p className='footer-location mb-1'>Ngong Lane, Ngong Lane Plaza, 1st Floor, Nairobi Kenya</p>
           <div className='contact'>
-            <p>+254 712 293 878 (Admin/General Enquiries)</p>
-            <p>+254 741 493 5656 (Data Science)</p>
-            <p>+254 712 293 878 (Whatsapp)</p>
-            <p>contact@moringaschool.com</p>
-            <p>admissions@moringaschool.com</p>
-            <p> P.O Box 28860 - 00100, Nairobi </p>
+            <p className='mb-1'>+254 712 293 878 (Admin/General Enquiries)</p>
+            <p className='mb-1'>+254 741 493 5656 (Data Science)</p>
+            <p className='mb-1'>+254 712 293 878 (Whatsapp)</p>
+            <p className='mb-1'>contact@moringaschool.com</p>
+            <p className='mb-1'>admissions@moringaschool.com</p>
+            <p className='mb-1'> P.O Box 28860 - 00100, Nairobi </p>
           </div>
         </div>
       </section>
@@ -198,7 +214,6 @@ const JobListing = () => {
       </div>
 
     </div>
-
 
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 // import Register from "./register"
-// import JobListing from "./job_listing"
+import JobListing from "./job_listing"
 // import JobInfo from "./job_listing2"
 // import ApplyModal from "./applymodal"
 // import ResetPassword from "./resetPassword2"
 // import ResetPassword from "./resetPassword1"
 // import Dashboard from "./dashboard"
 // import AlumniProfileMain from "./alumniProfileMain"
- import JobInfo from "./job_listing2"
+//  import JobInfo from "./job_listing2"
 // import AlumniNavbar from "../../components/AlumniNavbar"
 // import AlumniFooter from "../../components/AlumniFooter"
 // import SignIn from "./signin"
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main>
     <div >
-    <JobInfo />
+    <JobListing/>
     </div>
     </main>
   )
